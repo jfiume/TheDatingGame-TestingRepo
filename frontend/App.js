c27a97a18bf82f1bfc859007d2908faca2dd756a
ccomings <ccomings@gmail.com>
2017-08-07 19:00:57 -0700

    finish index.ios.js

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import SignUp from './components/signUp.js';
+
+const App = () => {
+  return (
+    <View>
+      <SignUp />
+    </View>
+  );
+};
+
+export default App;
