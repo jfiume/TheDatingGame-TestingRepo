895ee99d44c0b797f2901ef928d1aba3b4c5ef96
ccomings <ccomings@gmail.com>
2017-08-07 18:01:00 -0700

    create auth controller

---
+++ controllers/auth_controller.js
@@ -0,0 +1,52 @@
+const jwt = require('jwt-simple');
+const User = require('../model/user');
+const axios = require('axios');
+const SECRET = require('../config').SECRET;
+
+function tokenForUser (user) {
+  var obj = {
+    sub: user._id,
+    iat: new Date().getTime()
+  };
+  return jwt.encode(obj, SECRET);
+}
+
+exports.requireAuth = function(req, res, next) {
+  var authHeader = req.get('Authorization');
+  var jwtToken = jwt.decode(authHeader, SECRET);
+  var user_id = jwtToken.sub;
+  User.findById(user_id, function(err, user) {
+    if (err) { return next(err); }
+    if (!user) { return next(new Error("User not found.")); }
+    req.user = user;
+    next();
+  });
+};
+
+exports.facebookAuth = function(req, res, next) {
+  var token = req.body.token;
+  axios.get(`https://graph.facebook.com/v2.8/me?fields=id,name,email&access_token=${token}`).then(function (response) {
+    var facebook_id = response.data.id;
+    var name = response.data.name;
+    var email = response.data.email;
+    User.find({facebook_id: response.data.id}, function(err, users) {
+      user = users[0];
+      if (err) { return next(err); }
+      if (!user) {
+        var user = new User({
+          facebook_id: facebook_id,
+          email: email,
+          name: name
+        });
+        user.save(function(err) {
+          if (err) { return next(err); }
+          res.json({token: tokenForUser(user)});
+        });
+      } else {
+        res.json({token: tokenForUser(user)});
+      }
+    });
+  }).catch(function(error) {
+    return next(error);
+  });
+};
