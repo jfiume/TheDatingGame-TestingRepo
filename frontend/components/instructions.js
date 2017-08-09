7ac8b9045e2cf03add6a068f73256639859cc297
Younis Hamalawy <younis.hamalawy@gmail.com>
2017-08-09 11:03:55 -0700

    more styling

--- frontend/components/instructions.js
+++ frontend/components/instructions.js
@@ -52,13 +52,15 @@ export default class Instructions extends Component {
               3
             </Text>
             <Text style={styles.instructionsText}>
-              and they start chatting,
+              they start chatting,
             </Text>
           </View>
           <View style={styles.instructionsline}>
-            <Text style={styles.instructionsNumber}>
-              4
-            </Text>
+            <View style={styles.instructionsNumberContainer}>
+              <Text style={styles.instructionsNumber}>
+                4
+              </Text>
+            </View>
             <Text style={styles.instructionsText}>
               then shit gets real!
             </Text>
@@ -112,57 +114,84 @@ const styles = StyleSheet.create({
   instructions: {
     alignSelf: 'center',
     backgroundColor: 'transparent',
-    marginTop: -90
+    marginTop: -70
   },
   instructionsText: {
-    fontSize: 17,
+    fontSize: 16,
     // textAlign: 'center',
     color: 'white',
     flexWrap: 'wrap',
-
+    width: 300,
     // fontWeight: 'bold',
     fontFamily: 'AvenirNext-Medium',
   },
   instructionsline: {
     flexDirection: 'row',
-    alignItems: 'center',
+    // alignItems: 'center',
     paddingTop: 5,
     paddingBottom: 15,
     paddingRight: 10,
     paddingLeft: 15,
+    marginLeft:10
+  },
+  instructionsNumberContainer: {
+    borderRadius: 7,
+    borderWidth: 1.5,
+    borderColor: 'white',
   },
   instructionsNumber: {
     fontSize: 15,
     textAlign: 'center',
-    color: 'yellow',
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: 'white',
+    color: '#f0f066',
+    // borderRadius: 7,
+    // borderWidth: 1.5,
+    // borderColor: 'white',
     // fontWeight: 'bold',
     fontFamily: 'AvenirNext-Medium',
-    paddingTop: 3,
-    paddingBottom: 3,
-    paddingRight: 6,
-    paddingLeft: 6,
-    marginRight: 7
+    // paddingTop: 10,
+    // paddingBottom: 3,
+    // paddingRight: 6,
+    // paddingLeft: 6,
+    // marginTop: 3,
+    marginRight: 15,
+    width: 23,
+    height: 23,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    // alignSelf: 'center',
   },
   next: {
     alignSelf: 'center',
     borderRadius: 50,
-    borderWidth: 2.5,
+    borderWidth: 2,
     borderColor: 'white',
     backgroundColor: 'transparent',
-    marginBottom: 40
+    marginBottom: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center'
   },
   nextButton: {
-    fontSize: 25,
+    fontSize: 35,
     textAlign: 'center',
     color: 'white',
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingRight: 10,
-    paddingLeft: 10,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    // paddingTop: 5,
+    // paddingBottom: 30,
+    marginBottom:4,
+    // marginTop:4,
+    marginRight:2,
+    marginLeft:2,
+    // paddingRight: 10,
+    // paddingLeft: 10,
     // fontWeight: 'bold',
+    width: 40,
+    height: 40,
+    borderRadius: 50,
     fontFamily: 'AvenirNext-Medium',
   }
 });
