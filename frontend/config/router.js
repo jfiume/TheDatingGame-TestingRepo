e370edb968c01ec17a700eba504f8c047cdcdcaa
Ryan Rolfes <ryanmrolfes@gmail.com>
2017-08-11 10:03:23 -0700

    Adds Matches to tab bar

--- frontend/config/router.js
+++ frontend/config/router.js
@@ -3,12 +3,16 @@ import { TabNavigator } from 'react-navigation';
 
 import Instructions from '../components/intro/Instructions';
 import ProspectDetail from '../components/prospect/ProspectDetail';
+import MatchIndex from '../components/match/matchIndex';
 
 export const Tabs = TabNavigator({
   Instructions: {
     screen: Instructions,
   },
-  ProspectDetail: {
+  Prospects: {
     screen: ProspectDetail,
+  },
+  Matches: {
+    screen: MatchIndex,
   }
 });
