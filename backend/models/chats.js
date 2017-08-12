388a0115563cf3716a349990cc7f8c830458638f
ccomings <ccomings@gmail.com>
2017-08-11 21:00:22 -0700

    prepare for push

--- backend/models/chats.js
+++ backend/models/chats.js
@@ -1,7 +1,7 @@
- import mongoose, { Schema } from 'mongoose';
-
- const chatsSchema = new Schema({
-   messages: Array,
-   user_ids: Array,
-   played_games: Array
- });
+ // import mongoose, { Schema } from 'mongoose';
+ //
+ // const chatsSchema = new Schema({
+ //   messages: Array,
+ //   user_ids: Array,
+ //   played_games: Array
+ // });
