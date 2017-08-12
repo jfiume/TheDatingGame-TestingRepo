08d5f66b8ba38471e6119af5aa9b01b6d55b288d
Joe Fiume <joefiume88@gmail.com>
2017-08-12 13:42:53 -0700

    preparing the branch for testing

--- socket.js
+++ socket.js
@@ -3,6 +3,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const request = require('request');
+const user = require('./models/user');
 
 
 app.get('/', function(req, res){
@@ -10,16 +11,18 @@ app.get('/', function(req, res){
 });
 
 
-let users = 1;
+
 io.on('connection', function(socket){
-  users++;
-  let user = {"user": users};
   socket.on('chat message', function(msg){
-    let message = {authorId: users, messageOriginationTime: Date.now(), messageContent: msg};
+    let message = {
+      user: user,
+      messageOriginationTime: Date.now(),
+      messageContent: msg
+    };
     io.emit('chat message', msg);
     request({
       method: 'POST',
-      url: 'http://localhost:3000/v1/message',
+      url: location.origin + '/message',
       json: true,
       headers: {
           "Content-Type": "application/json",
